616c58f61d9f231b983709d40249260fa11e6c97
VGorash <gorash590@gmail.com>
2023-03-06 13:27:18 +0300

    simple threads

---
+++ main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <time.h>
+#include <pthread.h>
+
+#define size_n 5
+#define size_m 5
+
+int matrix_a[size_n][size_m];
+int matrix_b[size_n][size_m];
+int matrix_c[size_n][size_m] = {0};
+int matrix_d[size_n][size_m] = {0};
+
+int i_global = 0;
+
+void init_matrix(int matrix[size_n][size_m]){
+    for (int i=0; i<size_n; i++){
+        for (int j=0; j<size_m; j++){
+            matrix[i][j] = i * size_n + j + 1;
+        }
+    }
+}
+
+void print_matrix(int matrix[size_n][size_m]){
+    for(int i=0; i<size_n; i++){
+        for(int j=0; j<size_n; j++){
+            printf("%i ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void* multiply_row(void* params){
+    int i = i_global++;
+    for (int j=0; j<size_m; j++){
+        for (int k=0; k<size_m; k++){
+            matrix_c[i][j] += matrix_a[i][k] * matrix_b[k][j];
+        }
+    }
+    return NULL;
+}
+
+int main(){
+    pthread_t threads[size_n];
+    init_matrix(matrix_a);
+    init_matrix(matrix_b);
+    clock_t begin = clock();
+    for (int i=0; i<size_n; i++){
+        pthread_create(&threads[i], NULL, multiply_row, NULL);
+    }
+    for (int i=0; i<size_n; i++){
+        pthread_join(threads[i], NULL);
+    }
+    clock_t end = clock();
+    double exec_time = (double) (end - begin);
+    // print_matrix(matrix_c);
+    printf("Execution time: %f\n", exec_time);
+
+    begin = clock();
+    for (int i=0; i<size_n; i++){
+        for (int j=0; j<size_m; j++){
+            for (int k=0; k<size_m; k++){
+                matrix_d[i][j] += matrix_a[i][k] * matrix_b[k][j];
+            }
+        }
+    }
+    end = clock();
+
+    // print_matrix(matrix_d);
+    exec_time = (double) (end - begin);
+    printf("Execution time: %f\n", exec_time);
+}
+
+
+
